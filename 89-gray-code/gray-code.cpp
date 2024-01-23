a01ec73faaf9cb08d8e82e87e7dc55a55b167f9c
Priyans4475 <117758759+Priyans4475@users.noreply.github.com>
2024-01-24 01:01:32 +0530

    Time: 10 ms (33.07%) | Memory: 13 MB (21.95%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>ans;
         // n*2 = 
 
-        for(int i=0;i<(1<<n);i++)
+        for(int i=0;i<(pow(2,n));i++)
         {
             ans.push_back(i^(i/2));
         }
