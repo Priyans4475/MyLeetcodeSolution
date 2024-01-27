4e568d6fef74081f18073e394e12af6b0414b798
Priyans4475 <117758759+Priyans4475@users.noreply.github.com>
2024-01-27 22:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (9.69%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -4,26 +4,13 @@ public:
         if(dividend==INT_MIN && divisor==-1)
         return INT_MAX;
 
-        long long sign=(divisor>0)^(dividend>0)==0?1:-1;
+        long long ans=dividend/divisor;
+        if(ans>INT_MAX)
+        return INT_MAX;
+        if(ans<INT_MIN)
+        return INT_MIN;
 
-        long long divi=labs(dividend);
-         long long div=labs(divisor);
-         int res=0;
-
-         while(div<=divi)
-         {
-             long long temp=div,multi=1;
-             while(temp<<1 <=divi)
-             {
-                 temp=temp<<1;
-                 multi=multi<<1;
-             }
-
-             divi=divi-temp;
-             res=res+multi;
-         }
-
-         return sign*res;
+        return ans;
 
     }
 };
