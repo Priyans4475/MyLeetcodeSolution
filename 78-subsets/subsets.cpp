21ab0a26198d1c615f8416a68c94be65228d53bb
Priyans4475 <117758759+Priyans4475@users.noreply.github.com>
2024-02-27 00:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (59.35%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-vector<vector<int>>ans;
-    void f(vector<int>temp,vector<int>& nums,int i)
-    {
-      if(i==nums.size())
-      {ans.push_back(temp);
-      return;}
-      
-
-      temp.push_back(nums[i]);
-      f(temp,nums,i+1);
-      temp.pop_back();
-      f(temp,nums,i+1);
-      return;
-
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>temp;
-        f(temp,nums,0);
-        
+        vector<vector<int>>ans;
+        int n=nums.size();
+        for(int i=0;i<(1<<n);i++)
+        {
+            vector<int>temp;
+            for(int j=0;j<n;j++)   
+            {
+                if(i & (1<<j))
+                temp.push_back(nums[j]);
+            }
+
+            ans.push_back(temp);
+        }
+
         return ans;
     }
 };
+
+// for 7 -> 111 -> we have set bit at 0th, 1th and 2th bit->123
